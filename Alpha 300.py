27420abd9e18331f523113e638d884eab93f74eb
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2021-12-29 12:16:48 -0500

    Python Alpha 300

---
+++ Alpha 300.py
@@ -0,0 +1,57 @@
+
+'''              بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ            '''
+
+
+'''Alpha 300'''
+
+#conditional statements
+
+if 2 > 1:
+    print("This is a true statement")
+    
+var1 = 1
+var2 = 2
+
+if var2 > var1:
+    print("This is also True")
+    
+var3 = 12
+var4 = 44
+
+if var3 > var4:
+    print("this is true")
+else:
+    print("this is false")
+    
+value  = 18
+
+if value < 10:
+    print("That's a great deal!")
+elif 10 <= value <=20:
+    print("I'd still pay that")
+else:
+    print("Wow thats too much")
+    
+x = 10
+y = 20
+
+if x < 10 or y>15:
+    print("this is a true statement")
+
+if x == 10 and y == 16:
+    print("its a true statement")
+else: 
+    print("Its a false statement")
+    
+my_list = [1,2,3,4,5]
+
+if 7 not in my_list:
+    print("7 is not in my list")
+else: 
+    print("7 is in my list")
+    
+z = 5
+if 10 not in my_list and z !=15:
+    print('the above statement is true')
+else: 
+    print('the above statement is false')
